f512486570c4bddb5ce8af74352196d010342cbf
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-02-12 18:45:48 +0200

    Now everything is working. need to work on css

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,47 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import InputForm from "../components/InputForm"
+import InputForm from "../components/InputForm";
 
-
-
-
-interface FetchinStates { 
-    city: string ,
-    data: number|undefined
-    temperature: number | undefined,
-    humidity: number | undefined,
+interface FetchinStates {
+  city: string;
+  data: number | undefined;
+  temperature: number | undefined;
+  humidity: number | undefined;
 }
 
 export default class SearchBar extends Component<{}, FetchinStates> {
-
-state={
+  state = {
     city: "",
     data: undefined,
     temperature: undefined,
     humidity: undefined,
-   
-}
+  };
 
-async getWeather(e: any) {
-e.preventDefault();
-const city= e.target.elements.city.value
-const fetchWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7455b647c283cf033bf6c77821d011ee&units=metric`)
-const data = await fetchWeather.json()
-console.log(data)
-this.setState({temperature: data.main.temp, humidity: data.main.humidity})
+   getWeather=async(e: any)=>{
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const fetchWeather = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7455b647c283cf033bf6c77821d011ee&units=metric`
+    );
+    const data = await fetchWeather.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+    });
+    // this.setState(data)
+  }
 
-}
-
-    render() {
-        return (
-            <div>
-                <InputForm getWeather={this.getWeather}/>
-
-                {<p>Temperature: {this.state.temperature}</p>}
-                {<p>Humidity: {this.state.humidity}</p>}
-               
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <InputForm getWeather={this.getWeather} />
+        {<p>Temperature: {this.state.temperature} </p>}
+        {<p>Humidity: {this.state.humidity}</p>}
+      </div>
+    );
+  }
 }
